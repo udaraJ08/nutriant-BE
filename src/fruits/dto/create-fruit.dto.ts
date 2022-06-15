12071d7b1b502881ef619c3cfca800c44c7e4452
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2022-06-15 07:45:01 +0530

    schemas and dto created

--- src/fruits/dto/create-fruit.dto.ts
+++ src/fruits/dto/create-fruit.dto.ts
@@ -1,1 +1,19 @@
-export class CreateFruitDto {}
+export class CreateFruitDto {
+  readonly name: string;
+  readonly color: string;
+  readonly description: string;
+  readonly subTitle: string;
+  readonly general: {
+    readonly gen_name: string;
+    readonly gen_genes: string;
+    readonly gen_family: string;
+    readonly gen_order: string;
+  };
+  readonly nutrition: {
+    readonly carbohydrates: number;
+    readonly protein: number;
+    readonly fat: number;
+    readonly calories: number;
+    readonly sugar: number;
+  };
+}
--- src/fruits/entities/fruit.entity.ts
+++ src/fruits/entities/fruit.entity.ts
@@ -1,1 +1,33 @@
-export class Fruit {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type CatDocument = Fruit & Document;
+
+@Schema()
+export class Fruit {
+  @Prop({ required: true })
+  readonly name: string;
+  @Prop({ required: true })
+  readonly color: string;
+  @Prop({ required: true })
+  readonly description: string;
+  @Prop({ required: true })
+  readonly subTitle: string;
+  @Prop({ required: true })
+  readonly general: {
+    readonly gen_name: string;
+    readonly gen_genes: string;
+    readonly gen_family: string;
+    readonly gen_order: string;
+  };
+  @Prop({ required: true })
+  readonly nutrition: {
+    readonly carbohydrates: number;
+    readonly protein: number;
+    readonly fat: number;
+    readonly calories: number;
+    readonly sugar: number;
+  };
+}
+
+export const FruitSchema = SchemaFactory.createForClass(Fruit);
